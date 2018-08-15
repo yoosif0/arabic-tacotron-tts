ad96e7c8c371b96f1af2107872b760b6a847e796
Youssef Sherif <sharief@aucegypt.edu>
2018-08-15 23:16:07 +0200

    Update max_iters to prevent an error in Nawar Halabi's speech corpus training

--- hparams.py
+++ hparams.py
@@ -35,7 +35,7 @@ hparams = tf.contrib.training.HParams(
   use_cmudict=False,  # Use CMUDict during training to learn pronunciation of ARPAbet phonemes
 
   # Eval:
-  max_iters=200,
+  max_iters=400,
   griffin_lim_iters=60,
   power=1.5,              # Power to raise magnitudes to prior to Griffin-Lim
 )
